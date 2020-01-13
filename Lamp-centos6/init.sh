1cd9746febf21839eac99031361cf070192a7753
guansf <guansf1@qq.com>
2020-01-13 21:35:53 +0800

    Create init.sh

---
+++ Lamp-centos6/init.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+/etc/init.d/mysqld start
+mysql -e "grant all on *.* to root@'%' identified by '123';grant all on *.* to discuz@'%' identified by 'discuz';create database d
+iscuz charset utf8;"
+/etc/init.d/httpd start
+/usr/sbin/sshd -D
